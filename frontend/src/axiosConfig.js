884486cd5c506a9d11ed66b574dd199e87f1ebec
Aayush kumar <54721220+Aayush766@users.noreply.github.com>
2025-06-13 00:19:32 +0530

    Update axiosConfig.js

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const apiClient = axios.create({
 
-  baseURL: 'http://vercelfull.onrender.com/api', // Your backend API base URL
+  baseURL: 'https://vercelfull.onrender.com/api', // Your backend API base URL
   withCredentials: true, // This is CRUCIAL for sending HTTP-only cookies
 
   headers: {
