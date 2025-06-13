93ceb5b555271eb84c2057ddbcac042b9e77b0dc
Aayush kumar <54721220+Aayush766@users.noreply.github.com>
2025-06-13 10:03:47 +0530

    Update authRoutes.js

--- backendUp/routes/authRoutes.js
+++ backendUp/routes/authRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const { login, loginAdmin, loginTrainer, logout, forgotPasswordRequest, verifyDobAndSendLink, resetPassword } = require('../controllers/authController');
 const { getMe } = require('../controllers/userManagementController'); // Import getMe
@@ -10,10 +11,18 @@ router.post('/admin/login', loginAdmin); // Dedicated admin login route
 router.post('/trainer/login', loginTrainer); // Dedicated trainer login route
 router.post('/logout', logout);
 
+
+
 // NEW: Forgot Password Routes
+
 router.post('/forgot-password-request', forgotPasswordRequest); // Step 1: Email and user name check
+
 router.post('/verify-dob-and-send-link', verifyDobAndSendLink); // Step 2: DOB verification and send email
+
 router.put('/reset-password/:token', resetPassword); // Step 3: Reset password with token
+
 router.get('/me', auth, getMe);
+
+
 
 module.exports = router;
